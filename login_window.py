19875ec49344a3eef91836b24413eb92a2667ce9
szybkimedyk123 <masnax7xd@gmail.com>
2023-07-10 22:06:58 +0200

    New option: combobox with popular sites or typing your own mail.

--- login_window.py
+++ login_window.py
@@ -1,4 +1,5 @@
 from PySide2.QtWidgets import *
+import json
 import sys
 
 class LoginMenu(QWidget):
@@ -24,16 +25,112 @@ class LoginMenu(QWidget):
         password_input.addWidget(self.text_password)
         email_layout.addLayout(password_input)
 
+        '''
+        self.radio_option = QHBoxLayout()
+        self.settings_list = QRadioButton("List")
+        self.settings_text = QRadioButton("More options")
+        self.radio_option.addWidget(self.settings_list)
+        self.radio_option.addWidget(self.settings_text)
+        email_layout.addLayout(self.radio_option)
+        self.settings_list.toggled.connect(self.radio_action)
+        self.settings_text.toggled.connect(self.radio_action)
+        '''
+        self.radio_advance = QGridLayout()
+        self.popular_sites = QComboBox()
+        self.popular_sites.addItem("None")
+
+        with open('email_servers.json') as file:
+            self.data = json.load(file)
+
+        addresses = [item['name'] for item in self.data['address']]
+        self.popular_sites.addItems(addresses)
+
+        self.address_data = {item['name']: item for item in self.data['address']}
+
+        self.popular_sites.currentIndexChanged.connect(self.update_data)
+
+        self.radio_advance.addWidget(self.popular_sites, 0, 0, 1, 2)
+        email_layout.addLayout(self.radio_advance)
+
+        self.label_imap_host = QLabel("IAMP Host:")
+        self.text_imap_host = QLineEdit()
+        self.label_smtp_host = QLabel("STMP Host:")
+        self.text_smtp_host = QLineEdit()
+        self.label_imap_port = QLabel("IAMP Port:")
+        self.text_imap_port = QLineEdit()
+        self.label_smtp_port = QLabel("SMTP Port:")
+        self.text_smtp_port = QLineEdit()
+
+        self.radio_advance.addWidget(self.label_imap_host, 1, 0)
+        self.radio_advance.addWidget(self.text_imap_host, 1, 1)
+        self.radio_advance.addWidget(self.label_smtp_host, 2, 0)
+        self.radio_advance.addWidget(self.text_smtp_host, 2, 1)
+        self.radio_advance.addWidget(self.label_imap_port, 3, 0)
+        self.radio_advance.addWidget(self.text_imap_port, 3, 1)
+        self.radio_advance.addWidget(self.label_smtp_port, 4, 0)
+        self.radio_advance.addWidget(self.text_smtp_port, 4, 1)
+
+        buttons_group = QHBoxLayout()
         self.button_login = QPushButton("Login")
         self.button_login.clicked.connect(self.login)
-        email_layout.addWidget(self.button_login)
+        buttons_group.addWidget(self.button_login)
+        self.button_clear = QPushButton("Clear")
+        self.button_clear.clicked.connect(self.clear)
+        buttons_group.addWidget(self.button_clear)
+        email_layout.addLayout(buttons_group)
 
         self.setLayout(email_layout)
 
     def login(self):
-            user_email = self.text_email
-            user_password = self.text_password
+        user_email = self.text_email
+        user_password = self.text_password
 
+    def clear(self):
+        self.text_email.clear()
+        self.text_password.clear()
+        self.text_imap_host.clear()
+        self.text_imap_port.clear()
+        self.text_smtp_host.clear()
+        self.text_smtp_port.clear()
+
+    def update_data(self, index):
+        selected_value = self.popular_sites.currentText()
+
+        if selected_value == "None":
+            self.text_imap_host.setText("")
+            self.text_smtp_host.setText("")
+            self.text_imap_port.setText("")
+            self.text_smtp_port.setText("")
+
+        else:
+            selected_address = self.address_data.get(selected_value)
+
+            if selected_address:
+                imap_host = selected_address['imap_host']
+                smtp_host = selected_address['smtp_host']
+                imap_port = selected_address['imap_port']
+                smtp_port = selected_address['smtp_port']
+                self.text_imap_host.setText(imap_host)
+                self.text_smtp_host.setText(smtp_host)
+                self.text_imap_port.setText(imap_port)
+                self.text_smtp_port.setText(smtp_port)
+
+    '''
+    def radio_action(self):
+        if self.settings_list.isChecked():
+            self.radio_advance_text.setEnabled(False)
+            self.radio_advance_list.setEnabled(True)
+        elif self.settings_text.isChecked():
+            self.radio_advance_text.setEnabled(True)
+            self.radio_advance_list.setEnabled(False)
+    '''
+
+    @staticmethod
+    def load_names():
+        with open('email_servers.json') as file:
+            data = json.load(file)
+            address = [item['name'] for item in data['address']]
+            return address, data
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
